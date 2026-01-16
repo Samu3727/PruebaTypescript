59a66cb62b256fc87a5bd815bd8a883b29deb71c
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-16 12:31:28 -0500

    Feat: Constante del Login con los campos del useState agregados

--- prueba/src/Components/Login/Login.tsx
+++ prueba/src/Components/Login/Login.tsx
@@ -1,2 +1,13 @@
 import { useState  } from "react";
 import '../Styles/Login.css';
+
+export const Login = () =>{
+
+    const [correo, setCorreo] = useState('');
+    const [contrasena, setContrasena] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+
+        e.preventDefault();
+    }
+}
