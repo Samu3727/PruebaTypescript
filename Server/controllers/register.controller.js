3a74a11c0fd6ceb520434e67230d8cf5f03f6661
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-19 11:13:07 -0500

    Feat: Controlador del Registro terminado

--- Server/controllers/register.controller.js
+++ Server/controllers/register.controller.js
@@ -1,4 +1,9 @@
 export const register = async (req, res) => {
 
-    
+    try {
+        const { correo, contrasena } = req.body;
+        res.status(201).json({ message: 'Usuario registrado exitosamente' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error en el servidor' });
+    }
 }
