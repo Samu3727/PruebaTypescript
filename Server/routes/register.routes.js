96fa7afc78ae3fae4cb675c0e9e45895fc8bbe8b
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-19 11:04:26 -0500

    Feat: Ruta del Registro terminada

--- Server/routes/register.routes.js
+++ Server/routes/register.routes.js
@@ -1,3 +1,9 @@
 import express from 'express';
 import { loginController } from '../controllers/login.controller.js';
 import { register } from '../controllers/login.controller.js';
+
+const router = express.Router();
+
+router.post('/', loginController.login);
+
+export default router;
