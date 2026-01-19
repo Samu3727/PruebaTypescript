5e52661434dfdd4b90762947750758706d1032a6
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-19 10:35:54 -0500

    Feat: Llamado al puerto 3000 y uso de Express y Cors agregado

--- Server/index.js
+++ Server/index.js
@@ -2,3 +2,9 @@ import { express } from 'express';
 import { cors } from 'cors';
 import registerRoutes from './routes/register.routes.js';
 import loginRoutes from './routes/login.routes.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use (express.json());
+app.use (cors());
+
