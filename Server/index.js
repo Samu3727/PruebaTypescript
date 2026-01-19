ff752e2b43bb2f1259f9bf25c5357c6477b339e7
MauricioCordoba-Dev <carlosmauricioelcrack@gmail.com>
2026-01-19 14:24:54 -0500

    feat: Configuración del servidor Express con middlewares y rutas

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,26 @@
-import { express } from 'express';
-import { cors } from 'cors';
+import express from 'express';
+import cors from 'cors';
 import registerRoutes from './routes/register.routes.js';
 import loginRoutes from './routes/login.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use (express.json());
-app.use (cors());
 
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Rutas
 app.use('/api/register', registerRoutes);
-app.use('api/login', loginRoutes);
+app.use('/api/login', loginRoutes);
+
+
 
 app.listen(PORT, () => {
-
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`📝 Registro: POST http://localhost:${PORT}/api/register`);
+    console.log(`🔐 Login: POST http://localhost:${PORT}/api/login`);
+    console.log(`💓 Health: GET http://localhost:${PORT}/api/health`);
 });
 
 export default app;
