04bb037aaadbd8759fa01c3e7ba9b6dedabeb8d3
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-19 10:42:33 -0500

    Feat: Mensaje de escucha al servidor agregado

--- Server/index.js
+++ Server/index.js
@@ -11,3 +11,7 @@ app.use (cors());
 app.use('/api/register', registerRoutes);
 app.use('api/login', loginRoutes);
 
+app.listen(PORT, () => {
+
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
