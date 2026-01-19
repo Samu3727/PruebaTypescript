4a4b4f9c0b370cbaf0aa38c9c8c5e892f69d131f
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-19 10:59:02 -0500

    Feat: Ruta del Login terminada

--- Server/routes/login.routes.js
+++ Server/routes/login.routes.js
@@ -1,3 +1,9 @@
 import express from 'express';
 import { loginController } from '../controllers/login.controller.js';
 import { login } from '../controllers/login.controller.js';
+
+const router = express.Router();
+
+router.post('/', loginController.login);
+
+export default router;
